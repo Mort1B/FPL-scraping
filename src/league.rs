7c8f1df0dbf51552c2544da11feba67034077633
Morten Bergseng <bergseng.m@gmail.com>
2022-10-11 14:43:37 +0200

    major refactor + outputing graph

---
+++ src/league.rs
@@ -0,0 +1,47 @@
+  
+pub async fn get_league_data() -> Result<(), String> {
+    // Getting League info from API
+      let league = reqwest::Client::new()
+          .get("https://fantasy.premierleague.com/api/leagues-classic/41686/standings/")
+          .send()
+          .await
+          .unwrap()
+          .text()
+          .await
+          .unwrap();
+  
+      let json: Value = serde_json::from_str(&league).unwrap();
+      let players = json.get("standings").unwrap().get("results").unwrap();
+  
+  
+      // data storage league data
+      let mut by_points = HashMap::new();
+      let mut by_position = HashMap::new();
+  
+      for player in players.as_array() {
+          for x in player {
+              let previous_pos = x.get("last_rank").unwrap().as_i64().unwrap();
+              let this_pos = x.get("rank_sort").unwrap().as_i64().unwrap();
+              let spiller = x.get("player_name").unwrap().as_str().unwrap();
+              let total = x.get("event_total").unwrap().as_i64().unwrap();
+              let endring = previous_pos - this_pos;
+  
+              //iterates over each user per gameweek events is gameweek??? ---- need to take this out od the loop will be way easier, hardcode
+              // each player and have input for team ID
+  
+              by_points.insert(total, spiller);
+              by_position.insert(endring, spiller);
+          }
+      }
+
+
+          // SORTED BY POINTS WITH PLAYER
+    for key in by_points.keys().sorted().rev() {
+        println!("Poeng: {:?} -> {}", key, by_points[key])
+    }
+    // SORTED BY CHANGE IN LEAGUE POSITION WITH PLAYER
+    for key in by_position.keys().sorted().rev() {
+        println!("Endring: {} -> {}", key, by_position[key])
+    }
+
+} 
--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,103 @@
 use itertools::Itertools;
 use plotters::prelude::*;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
 // use std::io; // FOR writing for excel later
 // use std::process; for writing to excel later
 
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Root {
+    pub current: Vec<Current>,
+    pub past: Vec<Past>,
+    pub chips: Vec<Chip>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Current {
+    pub event: i64,
+    pub points: i64,
+    #[serde(rename = "total_points")]
+    pub total_points: i64,
+    pub rank: i64,
+    #[serde(rename = "rank_sort")]
+    pub rank_sort: i64,
+    #[serde(rename = "overall_rank")]
+    pub overall_rank: i64,
+    pub bank: i64,
+    pub value: i64,
+    #[serde(rename = "event_transfers")]
+    pub event_transfers: i64,
+    #[serde(rename = "event_transfers_cost")]
+    pub event_transfers_cost: i64,
+    #[serde(rename = "points_on_bench")]
+    pub points_on_bench: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Past {
+    #[serde(rename = "season_name")]
+    pub season_name: String,
+    #[serde(rename = "total_points")]
+    pub total_points: i64,
+    pub rank: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Chip {
+    pub name: String,
+    pub time: String,
+    pub event: i64,
+}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let mut wtr = csv::Writer::from_writer(io::stdout()); // EXCEL WRITER FOR LATER
 
+    //attempt 100 at chart
+    let root = BitMapBackend::new("plotters-net.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+
+    let mut ctx = ChartBuilder::on(&root)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .caption("Points on bench", ("sans-serif", 40))
+        .build_cartesian_2d(0..40, 0..150)
+        .unwrap();
+
+    ctx.configure_mesh().draw().unwrap();
+
     let team_id = 657266;
-
-    // Getting League info from API
-    let league = reqwest::Client::new()
-        .get("https://fantasy.premierleague.com/api/leagues-classic/41686/standings/")
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    let json: Value = serde_json::from_str(&league).unwrap();
-    let players = json.get("standings").unwrap().get("results").unwrap();
-
     // Base URL for player data
     let player_base = "https://fantasy.premierleague.com/api/entry/";
-
-    // data storage league data
-    let mut by_points = HashMap::new();
-    let mut by_position = HashMap::new();
-
-    for player in players.as_array() {
-        for x in player {
-            let previous_pos = x.get("last_rank").unwrap().as_i64().unwrap();
-            let this_pos = x.get("rank_sort").unwrap().as_i64().unwrap();
-            let spiller = x.get("player_name").unwrap().as_str().unwrap();
-            let total = x.get("event_total").unwrap().as_i64().unwrap();
-            let endring = previous_pos - this_pos;
-
-            //iterates over each user per gameweek events is gameweek??? ---- need to take this out od the loop will be way easier, hardcode
-            // each player and have input for team ID
-
-            by_points.insert(total, spiller);
-            by_position.insert(endring, spiller);
-        }
-    }
-
-    //data storage player data
-    let mut overall_rank_vec: Vec<i64> = Vec::new();
-    let mut in_bank_vec = Vec::new();
-    let mut event_transfers_vec = Vec::new();
-    let mut event_transfers_cost_vec = Vec::new();
-    let mut overall_rank_vec = Vec::new();
-    let mut points_gw_vec = Vec::new();
-    let mut points_on_bench_vec = Vec::new();
-    let mut total_points_vec = Vec::new();
-    let mut team_value_vec = Vec::new();
 
     let player_history = reqwest::Client::new()
         .get(player_base.to_string() + team_id.to_string().as_str() + "/history/")
         .send()
-        .await?
+        .await
+        .unwrap()
         .text()
-        .await?;
+        .await
+        .unwrap();
 
-    let json: Value = serde_json::from_str(&player_history).unwrap();
-    let events = json.get("current").unwrap();
-    for (ind, event) in events.as_array().iter().enumerate() {
-        for (ind, ev) in event.iter().enumerate() {
-            let in_bank = ev.get("bank").unwrap().as_f64().unwrap();
-            let event_transfers = ev.get("event_transfers").unwrap().as_i64().unwrap();
-            let event_transfers_cost = ev.get("event_transfers_cost").unwrap().as_i64().unwrap();
-            let overall_rank = ev.get("overall_rank").unwrap().as_i64().unwrap();
-            let points = ev.get("points").unwrap().as_i64().unwrap();
-            let points_on_bench = ev.get("points_on_bench").unwrap().as_i64().unwrap();
-            let rank = ev.get("rank").unwrap().as_i64().unwrap();
-            let rank_sort = ev.get("rank_sort").unwrap().as_i64().unwrap();
-            let total_points = ev.get("total_points").unwrap().as_i64().unwrap();
-            let value = ev.get("value").unwrap().as_f64().unwrap();
+    let mut pointsvec = Vec::new();
 
-            overall_rank_vec.push(rank);
-            in_bank_vec.push(in_bank);
-            event_transfers_vec.push(event_transfers);
-            event_transfers_cost_vec.push(event_transfers_cost);
-            overall_rank_vec.push(rank);
-            points_gw_vec.push(points);
-            points_on_bench_vec.push(points_on_bench);
-            total_points_vec.push(total_points);
-            team_value_vec.push(value);
-        }
+    let issues = serde_json::from_str::<Root>(&player_history).unwrap();
+    for i in issues.current {
+        // println!("{:?}, i", i);
+        println!("{:?}", i.points);
+        pointsvec.push(i.points_on_bench as i32)
     }
 
-    // SORTED BY POINTS WITH PLAYER
-    for key in by_points.keys().sorted().rev() {
-        println!("Poeng: {:?} -> {}", key, by_points[key])
-    }
-    // SORTED BY CHANGE IN LEAGUE POSITION WITH PLAYER
-    for key in by_position.keys().sorted().rev() {
-        println!("Endring: {} -> {}", key, by_position[key])
-    }
-
-    let tem_val = team_value_vec
-        .iter()
-        .map(|x| x / 10.0)
-        .collect::<Vec<f64>>();
-    // println!("{:?}", tem_val);
+    ctx.draw_series(LineSeries::new(
+        (0..).zip(pointsvec[..].iter()).map(|(idx, y)| (idx, *y)),
+        &BLUE,
+    ))
+    .unwrap();
+    root.present().unwrap();
 
     Ok(())
 }
