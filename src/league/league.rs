8483d9e08593e89aa84cba80bda79981c2210f07
Morten Bergseng <bergseng.m@gmail.com>
2022-10-14 10:49:11 +0200

    rem unwrap

--- src/league/league.rs
+++ src/league/league.rs
@@ -1,46 +1,40 @@
+use std::error::Error;
+
 use xlsxwriter::*;
 
 use super::leaguedata;
 
-pub async fn get_league_data() -> Result<(), String> {
+pub async fn get_league_data() -> Result<(), Box<dyn Error + Send + Sync>> {
     // Getting League info from API
     let league = reqwest::Client::new()
         .get("https://fantasy.premierleague.com/api/leagues-classic/41686/standings/")
         .send()
-        .await
-        .unwrap()
+        .await?
         .text()
-        .await
-        .unwrap();
+        .await?;
 
     //creating excel file
     let workbook = Workbook::new("test2.xlsx");
     let format_string = workbook.add_format().set_align(FormatAlignment::Justify);
-    let mut sheet2 = workbook.add_worksheet(None).unwrap();
-    sheet2.write_string(0, 0, "Player", None).unwrap();
-    sheet2.write_string(0, 2, "Posision cng", None).unwrap();
-    sheet2.write_string(0, 4, "Points rnd", None).unwrap();
+    let mut sheet2 = workbook.add_worksheet(None)?;
+    sheet2.write_string(0, 0, "Player", None)?;
+    sheet2.write_string(0, 2, "Posision cng", None)?;
+    sheet2.write_string(0, 4, "Points rnd", None)?;
 
     // Using struct data to selecting and printing to excel
-    let standings_root = serde_json::from_str::<leaguedata::Root>(&league).unwrap();
+    let standings_root = serde_json::from_str::<leaguedata::Root>(&league)?;
     for (i, res) in standings_root.standings.results.iter().enumerate() {
-        sheet2
-            .write_string(
-                i as u32 + 1,
-                0,
-                res.player_name.as_str(),
-                Some(&format_string),
-            )
-            .unwrap();
-        sheet2
-            .write_number(i as u32 + 1, 2, (res.last_rank - res.rank) as f64, None)
-            .unwrap();
-        sheet2
-            .write_number(i as u32 + 1, 4, res.event_total as f64, None)
-            .unwrap();
+        sheet2.write_string(
+            i as u32 + 1,
+            0,
+            res.player_name.as_str(),
+            Some(&format_string),
+        )?;
+        sheet2.write_number(i as u32 + 1, 2, (res.last_rank - res.rank) as f64, None)?;
+        sheet2.write_number(i as u32 + 1, 4, res.event_total as f64, None)?;
     }
 
-    workbook.close().unwrap();
+    workbook.close()?;
 
     Ok(())
 }
--- src/player/player.rs
+++ src/player/player.rs
@@ -9,10 +9,8 @@ pub async fn get_player_data() -> Result<(), Box<dyn Error + Send + Sync>> {
     // xlsxwriter, printing .png chart instead of charing in excel
     let workbook = Workbook::new("test1.xlsx");
     let format1 = workbook.add_format().set_font_color(FormatColor::Blue);
-    let mut sheet1 = workbook.add_worksheet(None).unwrap();
-    sheet1
-        .write_string(0, 0, "Points on Bench", Some(&format1))
-        .unwrap();
+    let mut sheet1 = workbook.add_worksheet(None)?;
+    sheet1.write_string(0, 0, "Points on Bench", Some(&format1))?;
 
     // Base URL for player data
     let team_id = 657266;
@@ -22,11 +20,9 @@ pub async fn get_player_data() -> Result<(), Box<dyn Error + Send + Sync>> {
     let player_history = reqwest::Client::new()
         .get(player_base.to_string() + team_id.to_string().as_str() + "/history/")
         .send()
-        .await
-        .unwrap()
+        .await?
         .text()
-        .await
-        .unwrap();
+        .await?;
 
     // Data storage
     let mut points_vec = Vec::new();
@@ -40,7 +36,7 @@ pub async fn get_player_data() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     // Using Deserialized structs to select data from API and loop through
     //to set data and write to excel file
-    let player_root = serde_json::from_str::<playerdata::Root>(&player_history).unwrap();
+    let player_root = serde_json::from_str::<playerdata::Root>(&player_history)?;
     for (i, curr) in player_root.current.iter().enumerate() {
         points_vec.push(curr.points as i32);
         rank_vec.push(curr.rank as i32);
@@ -51,65 +47,58 @@ pub async fn get_player_data() -> Result<(), Box<dyn Error + Send + Sync>> {
         event_transfer_cost_vec.push(curr.event_transfers_cost as i32);
         points_on_bench_vec.push(curr.points_on_bench as i32);
 
-        sheet1
-            .write_number(
-                i as u32 + 1,
-                0,
-                curr.points_on_bench.as_f64(),
-                Some(&format1),
-            )
-            .unwrap();
+        sheet1.write_number(
+            i as u32 + 1,
+            0,
+            curr.points_on_bench.as_f64(),
+            Some(&format1),
+        )?;
     }
 
-    draw_chart("points.png", "points", 0..40, 0..150, points_vec).unwrap();
+    // DRAWING CHARTS IN THIS REGION
 
-    draw_chart("gw_rank.png", "GW rank", 0..40, 0..10000000, rank_vec).unwrap();
+    draw_chart("points.png", "points", 0..40, 0..150, points_vec)?;
+    draw_chart("gw_rank.png", "GW rank", 0..40, 0..10000000, rank_vec)?;
     draw_chart(
         "overall_rank.png",
         "overall rank",
         0..40,
         0..8000000,
         overall_rank_vec,
-    )
-    .unwrap();
-    draw_chart("bank.png", "bank", 0..40, 0..50, bank_vec).unwrap();
-
+    )?;
+    draw_chart("bank.png", "bank", 0..40, 0..50, bank_vec)?;
     draw_chart(
         "team_value.png",
         "team_value",
         0..40,
         900..1100,
         team_value_vec,
-    )
-    .unwrap();
+    )?;
     draw_chart(
         "event_transfers.png",
         "event_transfers",
         0..40,
         0..10,
         event_transfers_vec,
-    )
-    .unwrap();
+    )?;
     draw_chart(
         "tranfer_cost.png",
         "transfer cost",
         0..40,
         0..20,
         event_transfer_cost_vec,
-    )
-    .unwrap();
+    )?;
     draw_chart(
         "points_on_bench.png",
         "points on bench",
         0..40,
         0..25,
         points_on_bench_vec,
-    )
-    .unwrap();
+    )?;
 
     // inserting plotters image in excel and writing the .xlsx file
-    sheet1.insert_image(2, 2, "points_on_bench.png").unwrap();
-    workbook.close().unwrap();
+    sheet1.insert_image(2, 2, "points_on_bench.png")?;
+    workbook.close()?;
 
     Ok(())
 }
@@ -123,24 +112,23 @@ fn draw_chart(
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     //Chart setup, printing image
     let root = BitMapBackend::new(path, (700, 480)).into_drawing_area();
-    root.fill(&WHITE).unwrap();
+    root.fill(&WHITE)?;
 
     let mut ctx = ChartBuilder::on(&root)
         .set_label_area_size(LabelAreaPosition::Left, 40)
         .set_label_area_size(LabelAreaPosition::Bottom, 40)
         .caption(caption, ("sans-serif", 40))
-        .build_cartesian_2d(x_spec, y_spec)
-        .unwrap();
+        .build_cartesian_2d(x_spec, y_spec)?;
 
-    ctx.configure_mesh().draw().unwrap();
+    ctx.configure_mesh().draw()?;
 
     //Drawing to the png
     ctx.draw_series(LineSeries::new(
         (0..).zip(data[..].iter()).map(|(idx, y)| (idx, *y)),
         &BLUE,
-    ))
-    .unwrap();
-    root.present().unwrap();
+    ))?;
+
+    root.present()?;
 
     Ok(())
 }
